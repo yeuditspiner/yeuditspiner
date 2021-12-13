4e4c086a8452a83d0aae386a12aa49c91d7219eb
yeuditspiner <93085182+yeuditspiner@users.noreply.github.com>
2021-12-13 23:46:49 +0200

    Add files via upload

---
+++ Placementstudents.cs
@@ -0,0 +1,65 @@
+﻿
+using DalPlacementStudent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bll
+{
+
+    enum marks { Exellent, Possible, Impossible }
+    public class Placementstudents
+    {
+
+        YeuditTablePlaceMentEntities db = new YeuditTablePlaceMentEntities();
+        public StudentTbl student { get; set; }
+        public Placementstudents() { 
+              
+        }
+        //בדוק אילוצים
+        public Placementstudents(StudentTbl s)
+        {
+            this.student = s;
+        }
+        public bool IsCointains(List<Constraints> constarints, string DetailTypical,bool yesOrNat)
+        {
+            
+            foreach (var item in constarints)
+            {
+               
+                if (item.Name == DetailTypical && Convert.ToBoolean(item.Value)==yesOrNat)
+                    return true;
+            }
+            return false;
+        }
+        public bool CheckConstraints(List<Constraints> constarints)
+        {
+            var r = db.TypicalToStudent_Tbl.Where(x => x.StudentID ==this.student.StudentID).ToList();//רשימת אילוצי תלמידה
+            foreach (var item in r)
+            {
+             
+                string  id = (db.TypicalTbl.Where(x => x.TypicalID == item.TypicalID)).Select(x=>x.DetailTypical.Trim()).First();
+                if (!IsCointains(constarints, id.Trim(), item.Status.Value))
+                {
+                    return false;
+                }
+               
+            }
+            return true;//אם הכל טוב 
+        }
+
+      
+        //בדוק מיקום
+        public string CheckLocation(int line)
+        {
+            if (this.student.AdviceableLine == line)
+                return marks.Exellent.ToString();
+            else
+            if(Math.Abs((decimal)(this.student.AdviceableLine - line)) == 1)
+             
+            return marks.Possible.ToString();
+            return marks.Impossible.ToString();
+        }
+
+    }
+}
---
+++ TablePlacement.cs
@@ -0,0 +1,37 @@
+﻿
+using DalPlacementStudent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bll;
+
+
+namespace Bll
+{
+   public  class TablePlacement
+    {
+        YeuditTablePlaceMentEntities db = new YeuditTablePlaceMentEntities();
+        public StudentTbl  s1 { get; set; }
+        public StudentTbl  s2 { get; set; }
+        public TypicalToStudent_Tbl t { get; set; }
+       public TablePlacement(StudentTbl s1,StudentTbl s2)
+        {
+           this. s1 = s1;
+           this. s2 = s2;
+           t = new TypicalToStudent_Tbl();
+        }
+
+
+        public string CompatibilityTest()
+        {
+            if ((s1.Chatter + s2.Chatter) > 5 && (s1.Chatter + s2.Chatter <= 7))
+                return marks.Possible.ToString();
+            if (s1.Chatter + s2.Chatter < 5)
+                return marks.Exellent.ToString();
+            return marks.Impossible.ToString();
+
+        }
+    }
+}
